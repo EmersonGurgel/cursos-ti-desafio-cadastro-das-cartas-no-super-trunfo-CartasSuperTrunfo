3133ac6ceb7f7770b2259774cc8c5be3cfa0b6b5
EmersonGurgel <202502613768@alunos.estacio.br>
2025-02-15 16:08:13 +0000

    coleta e impressao de dados feitas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -16,7 +16,53 @@ int main() {
     
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    // Exiba os valores inseridos para cada atributo da cidade, um por linha;
+     
+   
+ 
+     
+    // Definição de variáveis;
+    char estado;
+    char codigo[3];
+    char nomedacidade[20];
+    int populacao;
+    float area;
+    float pib;
+    int pontosturisticos;
+    
+    //titulo de apresentação;
+    printf("Desafio super trunfo em C\n");
 
-    return 0;
+    // dizendo ao usuario as informações que serão coletadas e coletando elas;
+    printf("Digite o estado:\n");
+    scanf("%c", &estado);
+    
+    printf("Digite o código\n");
+    scanf("%s", &codigo);
+    
+    print("Digite o nome da cidade\n");
+    scanf("%s", &nomedacidade);
+
+    print("Digite a população\n");
+    scanf("%d", &populacao);
+
+    printf("Digite a área\n");
+    scanf("%f", &area);
+
+    printf("Digite o pib\n");
+    scanf("%f", &pib);
+
+    printf("Digite o número de pontos turísticos\n");
+    scanf("%d", &pontosturisticos);
+
+    //Nessa parte abaixo será imprimido os dados que foram coletados do usuario;
+    printf("Estado: %c", estado);
+    printf("Código da cidade: %s", codigo);
+    printf("Nome da cidade: %s", nomedacidade);
+    printf("População: %d", populacao);
+    printf("Área: %f", area);
+    printf("Pib: %f", pib);
+    printf("Pontos turísticos: %d", pontosturisticos);
+
+
 }
